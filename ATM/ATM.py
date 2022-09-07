7f34db44585255b28bc3bd09c3ecea4ddefe60e6
aya <aya@anadeainc.com>
2022-09-07 16:24:18 +0300

    add .gitignore

--- ATM/ATM.py
+++ ATM/ATM.py
@@ -3,23 +3,24 @@ class ATM:
         self.name = name
         self.amount = amount
         self.dict_denomination = dict_denomination
-        self.balance = balance
 
-
-    def get_balance(self, balance=0):
+    def get_balance(self):
+        balance = 0
         for key, value in self.dict_denomination.items():
             balance += int(key) * value
         return balance
 
     def get(self, total):
-        if total < get_balance(self, balance=0):
-            balance -= total
-            print(balance())
+        balance = ATM.get_balance()
+        if total < balance:
+            return ATM.choice_of_banknotes(total)
         else:
             return print('Insufficient balance')
+
+    def choice_of_banknotes(self, total):
+        pass
 
 
 dict_denom = {'5': 5, '10': 5, '20': 5, '50': 5, '100': 5}
 my_card = ATM('aya', '12345678', dict_denom)
 print(my_card.get_balance())
-print(my_card.get(15))
