1c74345bafa086d7aaecbacdfd86939e569c28c4
aya <aya@anadeainc.com>
2022-09-08 11:36:27 +0300

    min choice of banknotes

--- ATM/ATM.py
+++ ATM/ATM.py
@@ -19,20 +19,28 @@ class ATM:
 
     def choice_of_banknotes(self, total):
         list_denom = [int(key) for key in self.dict_denomination.keys()]
-        res = []
-        k = total
-        while k != 0:
+        f = [1000] * (total + 1)
+        f[0] = 0
+        for k in range(1, total + 1):
             for i in range(len(list_denom)):
-                if k - list_denom[i] >= 0:
-                    res.append(list_denom[i])
-                    k -= list_denom[i]
-        print(res)
+                if k - list_denom[i] >= 0 and f[k - list_denom[i]] < f[k]:
+                    f[k] = f[k - list_denom[i]]
+            f[k] += 1
+        result = []
+        while total != 0:
+            for i in range(len(list_denom)):
+                if total - list_denom[i] >= 0 and f[total] == f[total - list_denom[i]] + 1:
+                    result.append(list_denom[i])
+                    total -= list_denom[i]
+        print(result)
 
 
 dict_denom = {'5': 5, '10': 5, '20': 5, '50': 5, '100': 5}
 my_card = ATM('aya', '12345678', dict_denom)
 print(my_card.get_balance())
 print(my_card.get(15))
+print(my_card.get(25))
+print(my_card.get(60))
 
 
 
