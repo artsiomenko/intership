173c78a80fb286d8584eeafea0f5e5b381849790
aya <aya@anadeainc.com>
2022-09-08 14:21:54 +0300

    ATM_not_empty

--- ATM/ATM.py
+++ ATM/ATM.py
@@ -17,29 +17,35 @@ class ATM:
         else:
             return print('Insufficient balance')
 
+    def ATM_not_empty(self):
+        return True if self.get_balance() > 0 else False
+
     def choice_of_banknotes(self, total):
         list_denom = [int(key) for key, value in self.dict_denomination.items() if self.dict_denomination[key] > 0]
         f = [1000] * (total + 1)
         f[0] = 0
         for k in range(1, total + 1):
             for i in range(len(list_denom)):
-                if k - list_denom[i] >= 0 and f[k - list_denom[i]] < f[k]:
-                    f[k] = f[k - list_denom[i]]
+                elem = list_denom[i]
+                if k - elem >= 0 and f[k - elem] < f[k]:
+                    f[k] = f[k - elem]
             f[k] += 1
         result = []
-        while total != 0:
+        while total != 0 and self.ATM_not_empty():
             for i in range(len(list_denom)):
-                if total - list_denom[i] >= 0 and f[total] == f[total - list_denom[i]] + 1:
-                    if self.dict_denomination[str(list_denom[i])] > 0:
-                        result.append(list_denom[i])
-                        total -= list_denom[i]
-                        self.dict_denomination[str(list_denom[i])] -= 1
+                elem = list_denom[i]
+                if total - elem >= 0 and f[total] == f[total - elem] + 1:
+                    if self.dict_denomination[elem] > 0:
+                        result.append(elem)
+                        total -= elem
+                        self.dict_denomination[elem] -= 1
                     else:
+
                         continue
         return result
 
 
-dict_denom = {'5': 5, '10': 5, '20': 5, '50': 5, '100': 5}
+dict_denom = {5: 5, 10: 5, 20: 5, 50: 5, 100: 5}
 my_card = ATM('aya', '12345678', dict_denom)
 print(my_card.get_balance())
 print(my_card.get(2500))
